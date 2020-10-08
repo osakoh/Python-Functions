ee9c037d54d234f52fa62628930e4e265c7e85e2
Osas Michael <macmive@gmail.com>
2020-10-08 21:31:43 +0000

    implemented Student inheritance in WorkingStudent class

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -6,3 +6,27 @@ class Student:
 
     def average(self):
         return sum(self.marks)/len(self.marks)
+
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)  # calls Student __init__()
+        self.salary = salary
+
+    def salary_per_week(self):
+        return self.salary * 15
+
+    def salary_per_month(self):
+        return self.salary_per_week() * 4
+
+
+sau = WorkingStudent('Sau', 'ECE', 12.50)
+print(sau.salary)
+print(sau.salary_per_week())
+print(sau.salary_per_month())
+
+sau.marks.append(74)
+sau.marks.append(85)
+sau.marks.append(90)
+
+print(sau.average())
